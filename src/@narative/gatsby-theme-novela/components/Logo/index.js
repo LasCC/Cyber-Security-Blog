b6d870aaaf60bea77414bd7af01fded8c54fb3c8
Ludovic <erazjean@gmail.com>
2020-05-09 15:53:43 +0200

    Fix previous update (logo not working) 🤨

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Logo({ fill }) {
+  return (
+    <svg viewBox='0 0 106 28' height='30px'>
+      <path d='M62.9 12h2.8v10....' />
+      <path fill={fill} d='M25 14h-...' />
+      <path d='M14 0C6.3...' fill='#639' />
+    </svg>
+  );
+}
--- src/@narative/gatsby-theme-novela/src/styles/index.ts
+++
@@ -1,4 +0,0 @@
-import { globalStyles } from "@styles/global";
-import { media } from "@styles/media";
-
-export { globalStyles, media };
