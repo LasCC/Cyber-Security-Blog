55564e3263b6e55de5685a97117d6cacd5eb25a0
Ludovic <erazjean@gmail.com>
2020-05-10 01:09:21 +0200

    Fix metadata image 🤡

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     name: `Ludovic COULON`,
     siteUrl: `https://ludovic-cyber-sec.web.app`,
     description: `Blog that gathers different articles about computer security and writup of challenges I've done..`,
-    image: `https://i.imgur.com/BgQ8dIS.png`,
+    image: `./src/meta.png`,
     hero: {
       heading: `A place to drop off things that I find interesting and that others might enjoy...`,
       maxWidth: 680,
