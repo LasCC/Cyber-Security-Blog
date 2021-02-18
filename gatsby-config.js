af85b2c2b2aa938922c9f14e2a0e129253856588
Ludovic COULON <coulonludovicc@gmail.com>
2021-02-18 15:23:20 +0100

    Image siteMetaData update (please)

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
 			heading: `A place to drop off things that I find interesting and that others might enjoy...`,
 			maxWidth: 680
 		},
-		image: `public/preview.jpg`,
+		image: `preview.jpg`,
 		social: [
 			{
 				name: `github`,
