c53b49d310a5e8eb2fc28fab4e757a72c78938ee
LasCC <coulonludovicc@gmail.com>
2024-02-17 19:35:02 +0100

    Refactor header and recent posts components

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import type { Socials } from "./types";
 export const SITE_URL = "https://ludovic-coulon.com";
 export const SITE_TITLE = "Ludovic COULON - Cybersecurity blog";
 export const SITE_DESCRIPTION =
-  "Ludovic COULON - Cybersecurity blog | Blog that brings together various articles on IT security and write about challenges I have achieved...";
+  "Ludovic COULON - Cybersecurity blog | Blog that brings together various articles on IT security and writeup about challenges I have achieved.";
 export const SITE_LOGO = "/logo.svg";
 
 export const SOCIALS: Socials = [
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
+import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL } from "../config";
 
 export async function GET() {
   const posts = await getCollection("blog");
